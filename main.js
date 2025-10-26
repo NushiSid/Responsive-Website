a467b9a43c204a475c6111078e0bafa03f29b3aa
NushiSid <puja.ramani@gmail.com>
2025-10-26 10:57:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onresize=w
+window.onload=w
+window.onresize=h
+window.onload=h
+function w(){
+    var ws=window.innerWidth
+    document.getElementById("tlh4").innerHTML=ws+" pixels"
+    var hs=window.innerHeight
+    document.getElementById("lh4").innerHTML=hs+" pixels"
+    
+    
+}
